567ae3501d3a25bb31676c9649abbac455b40fdf
nipun gera <nipungera7777@gmail.com>
2020-09-26 12:15:50 +0530

    prog1

---
+++ p21.java
@@ -0,0 +1,28 @@
+int detail;
+MyException(int a)
+{
+	detail=a;
+}
+public string toString()
+{
+	return	 "detail";
+}
+}
+class p21
+{
+	static void compute (int a) throws MyException
+	{
+		throw new MyException(a);
+	}
+	public static void main(String[] args)
+	{
+		try
+		{
+			compute(3);
+		}
+		catch(MyException e)
+		{
+			System.out.print("Exception");
+		}
+	}
+}
