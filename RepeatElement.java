7a3b3ae88afbd1f8d84a1beede1a47a6f9595b81
harshmittal1750 <50164413+harshmittal1750@users.noreply.github.com>
2020-10-01 19:57:52 +0530

    Add files via upload

---
+++ RepeatElement.java
@@ -0,0 +1,19 @@
+class RepeatElement {
+    void printRepeating(int arr[], int size) {
+        int i, j;
+        System.out.println("Repeated Elements are :");
+        for (i = 0; i < size; i++) {
+            for (j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j])
+                    System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RepeatElement repeat = new RepeatElement();
+        int arr[] = { 4, 2, 4, 5, 2, 3, 1 };
+        int arr_size = arr.length;
+        repeat.printRepeating(arr, arr_size);
+    }
+}
